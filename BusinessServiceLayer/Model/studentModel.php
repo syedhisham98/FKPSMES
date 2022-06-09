86c098f9bf60691aca7c16b21eecedcc229d3edb
syedhisham98 <syedhisham5598@gmail.com>
2022-06-09 23:30:33 +0800

    Update studentModel.php

--- BusinessServiceLayer/Model/studentModel.php
+++ BusinessServiceLayer/Model/studentModel.php
@@ -1,38 +1,38 @@
 <?php
 require_once '../../BusinessServiceLayer/libs/db.php';
 
-class customerModel{
-    public $customer_id,$name,$email,$phone,$address,$city,$state,$zipcode,$username,$password,$usertype;
+class studentModel{
+    public $student_id,$name,$email,$phone,$username,$password,$usertype,$ID,$tittle;
     
-    function addCust(){
-        $sql = "insert into customer(customer_name, customer_email, customer_phone, customer_address, customer_city, customer_state, customer_zipcode, username, password, usertype) values(:name, :email, :phone, :address, :city, :state, :zipcode, :username, :password, :usertype)";
-        $args = [':name'=>$this->name, ':email'=>$this->email, ':phone'=>$this->phone, ':address'=>$this->address, ':city'=>$this->city, ':state'=>$this->state, ':zipcode'=>$this->zipcode, ':username'=>$this->username, ':password'=>$this->password, ':usertype'=>$this->usertype];
+    function addStudent(){
+        $sql = "insert into student(student_name, student_email, student_phone,username, password, usertype, matric_id, project_tittle) values(:name, :email, :phone,:username, :password, :usertype, :ID, :tittle)";
+        $args = [':name'=>$this->name, ':email'=>$this->email, ':phone'=>$this->phone,':username'=>$this->username, ':password'=>$this->password, ':usertype'=>$this->usertype, ':ID'=>$this->ID, ':tittle'=>$this->tittle];
         $stmt = DB::run($sql, $args);
         $count = $stmt->rowCount();
         return $count;
     }
     
-    function viewallCust(){
-        $sql = "select * from customer";
+    function viewallStudent(){
+        $sql = "select * from student";
         return DB::run($sql);
     }
     
-    function viewCustomer(){
-        $sql = "select * from customer where customer_id=:customer_id";
-        $args = [':customer_id'=>$this->customer_id];
+    function viewstudent(){
+        $sql = "select * from student where student_id=:student_id";
+        $args = [':student_id'=>$this->student_id];
         return DB::run($sql,$args);
     }
-    function viewCustomerFullAddress(){
-        $sql = "select concat(customer_address,', ',customer_city,', ',customer_state,', ',customer_zipcode) as fullAddress from customer where customer_id=:customer_id";
-        $args = [':customer_id'=>$this->customer_id];
+    function viewstudentFullAddress(){
+        $sql = "select concat(student_address,', ',student_city,', ',student_state,', ',student_zipcode) as fullAddress from student where student_id=:student_id";
+        $args = [':student_id'=>$this->student_id];
         $stmt = DB::run($sql,$args);
         $result = $stmt->fetchColumn();
         return $result;
     }
     
-    function modifyCustomer(){
-        $sql = "update customer set customer_name=:name,customer_email=:email,customer_phone=:phone,customer_address=:address,customer_city=:city,customer_state=:state,customer_zipcode=:zipcode,username=:username,password=:password where customer_id=:customer_id";
-        $args = [':customer_id'=>$this->customer_id,':name'=>$this->name, ':email'=>$this->email, ':phone'=>$this->phone, ':address'=>$this->address, ':city'=>$this->city, ':state'=>$this->state, ':zipcode'=>$this->zipcode,':username'=>$this->username,':password'=>$this->password];
+    function modifystudent(){
+        $sql = "update student set student_name=:name,student_email=:email,student_phone=:phone,username=:username,password=:password, project_tittle=:tittle ,matric_id=:ID where student_id=:student_id";
+        $args = [':student_id'=>$this->student_id,':name'=>$this->name, ':email'=>$this->email, ':phone'=>$this->phone,  ':username'=>$this->username,':password'=>$this->password,':ID'=>$this->ID,':tittle'=>$this->tittle];
         return DB::run($sql,$args);
     }
     
