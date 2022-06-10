a596ed34b94e787be45035ba9b3683c220112a6d
nurinsolehah <nurinsolehah28@gmail.com>
2022-06-10 08:08:13 +0800

    Update ViewProgress.php

--- ApplicationLayer/ManageProgressReport/ViewProgress.php
+++ ApplicationLayer/ManageProgressReport/ViewProgress.php
@@ -57,7 +57,7 @@ input {
         <div class="row">
           <div class="col-lg-12 col-md-12 col-sm-12">
             <div class="bradcaump__inner text-center">
-              <h2 class="bradcaump-title">Student Schedule</h2>
+              <h2 class="bradcaump-title">PSM Progress</h2>
               <nav class="bradcaump-inner">           
               </nav>
             </div>
